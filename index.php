b90a5a9b0c2da1482538112f6884745ee73e692f
kannan maharajan <kannan.maharajan61@gmail.com>
2021-05-13 11:35:48 +0530

    initial commit

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 include("project.html");
+<a href="file:///C:/Users/Lenovo/Documents/REAL%20ESTATE%20PROJ/project/website.html" name="real estate website" id="linktome" target="_blank"> my porject</a>
 ?>
